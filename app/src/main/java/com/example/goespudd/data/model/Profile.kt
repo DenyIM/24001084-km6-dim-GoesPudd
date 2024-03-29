8c5fa6e02bc109340cbfe7e4390026b6decca93e
Deny Iqbal Mubarok <denyiqbalmubarok@gmail.com>
2024-03-30 03:03:04 +0700

    Finishing Feature Profile for User

---
+++ app/src/main/java/com/example/goespudd/data/model/Profile.kt
@@ -0,0 +1,8 @@
+package com.example.goespudd.data.model
+
+data class Profile(
+    val username: String,
+    val email: String,
+    val phoneNumber: String,
+    val imgProfile: String
+)
--- app/src/main/java/com/example/goespudd/presentation/profile/ProfileFragment.kt
+++ app/src/main/java/com/example/goespudd/presentation/profile/ProfileFragment.kt
@@ -5,18 +5,58 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import coil.load
+import coil.transform.CircleCropTransformation
 import com.example.goespudd.R
 import com.example.goespudd.databinding.FragmentProfileBinding
 
 class ProfileFragment : Fragment() {
+
     private lateinit var binding: FragmentProfileBinding
+
+    private val viewModel: ProfileViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
         binding = FragmentProfileBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setClickListener()
+        observeEditMode()
+        observeProfileData()
+    }
+
+    private fun observeProfileData() {
+        viewModel.profileData.observe(viewLifecycleOwner) {
+            binding.ivImgUserProfile.load(it.imgProfile) {
+                crossfade(true)
+                error(R.drawable.ic_tab_profile)
+                transformations(CircleCropTransformation())
+            }
+            binding.usernameEditText.setText(it.username)
+            binding.emailEditText.setText(it.email)
+            binding.phoneNumberEditText.setText(it.phoneNumber)
+        }
+    }
+
+    private fun setClickListener() {
+        binding.btnEdit.setOnClickListener {
+            viewModel.changeEditMode()
+        }
+    }
+
+    private fun observeEditMode() {
+        viewModel.isEditMode.observe(viewLifecycleOwner) {
+            binding.emailEditText.isEnabled = it
+            binding.phoneNumberEditText.isEnabled = it
+            binding.usernameEditText.isEnabled = it
+        }
     }
 }
